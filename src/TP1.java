0ac00250f2a6c7d251dcd3f9e70ffe2b102bc8f4
lucasgcaldas <lggomes007@gmail.com>
2022-01-23 18:38:56 -0300

    Updating methods and adding matrix

--- src/TP1.java
+++ src/TP1.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class TP1 {
     public static void main(String[] args) {
 
+        String[][] temasPalavras = {
+                {"ALIMENTO", "ARROZ", "PIZZA", "LASANHA", "CHURRASCO", "FEIJAO", "BATATA", "STROGONOFF", "SALADA", "MACARRAO", "SANDUICHE"},
+                {"CIDADE", "BRASILIA", "LUZIANIA", "GOIANIA", "ANAPOLIS", "VALPARAISO", "GAMA", "CRISTALINA", "FORMOSA", "SOBRADINHO", "PLANALTINA"},
+                {"ANIMAL", "CAVALO", "GATO", "CACHORRO", "ABELHA", "GALINHA", "PATO", "OVELHA", "SAPO", "URSO", "FOCA"},
+                {"COR", "AZUL", "BRANCO", "VERMELHO", "VERDE", "PRETO", "ROSA", "LARANJA", "MARROM", "CINZA", "ROXO"},
+                {"NOME", "LUCAS", "BIANCA", "FELIPE", "SIMONE", "SIDNEY", "INGRID", "JOAO", "JENIFER", "RAFAEL", "LETICIA"}};
+
         menu();
     }
 
@@ -17,36 +24,40 @@ public class TP1 {
         int choose = sc.nextInt();
 
         switch (choose) {
-            case 1:
-                gerenciarTemas();
-                break;
-            case 2:
-                gerenciarPalavras();
-                break;
-            case 3:
-                jogar();
-                break;
-            case 4:
-                sair();
-                break;
-            default:
-                System.out.println("Opção Inválida");
+            case 1 -> gerenciarTemas();
+            case 2 -> gerenciarPalavras();
+            case 3 -> jogar();
+            case 4 -> sair();
+            default -> System.out.println("Opção Inválida");
         }
+
+        sc.close();
     }
 
     public static void gerenciarTemas() {
+        System.out.println("(a) Cadastro");
+        System.out.println("(b) Exclusão");
+        System.out.println("(c) Busca");
+
 
     }
 
     public static void gerenciarPalavras() {
-
+        System.out.println("(a) Cadastro");
+        System.out.println("(b) Exclusão");
+        System.out.println("(c) Busca");
+        System.out.println("(d) Listagem");
     }
 
     public static void jogar() {
+        System.out.println("(a) Seleção de tema");
+        System.out.println("(b) Jogo");
+        System.out.println("(c) Jogar novamente");
+        System.out.println("(d) Sair");
 
     }
 
     public static void sair() {
-
+        System.exit(1);
     }
 }
