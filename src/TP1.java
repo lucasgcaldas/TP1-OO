632b0d5d8def48cbf374d7c49593cf91bdb740cb
lucasgcaldas <lggomes007@gmail.com>
2022-01-25 00:33:59 -0300

    Adding all logic to gerenciarTemas method

--- src/TP1.java
+++ src/TP1.java
@@ -1,14 +1,18 @@
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TP1 {
-    public static void main(String[] args) {
 
-        String[][] temasPalavras = {
-                {"ALIMENTO", "ARROZ", "PIZZA", "LASANHA", "CHURRASCO", "FEIJAO", "BATATA", "STROGONOFF", "SALADA", "MACARRAO", "SANDUICHE"},
-                {"CIDADE", "BRASILIA", "LUZIANIA", "GOIANIA", "ANAPOLIS", "VALPARAISO", "GAMA", "CRISTALINA", "FORMOSA", "SOBRADINHO", "PLANALTINA"},
-                {"ANIMAL", "CAVALO", "GATO", "CACHORRO", "ABELHA", "GALINHA", "PATO", "OVELHA", "SAPO", "URSO", "FOCA"},
-                {"COR", "AZUL", "BRANCO", "VERMELHO", "VERDE", "PRETO", "ROSA", "LARANJA", "MARROM", "CINZA", "ROXO"},
-                {"NOME", "LUCAS", "BIANCA", "FELIPE", "SIMONE", "SIDNEY", "INGRID", "JOAO", "JENIFER", "RAFAEL", "LETICIA"}};
+    public static String[][] temasPalavras = {
+            {"ALIMENTO", "ARROZ", "PIZZA", "LASANHA", "CHURRASCO", "FEIJAO", "BATATA", "STROGONOFF", "SALADA", "MACARRAO", "SANDUICHE"},
+            {"CIDADE", "BRASILIA", "LUZIANIA", "GOIANIA", "ANAPOLIS", "VALPARAISO", "GAMA", "CRISTALINA", "FORMOSA", "SOBRADINHO", "PLANALTINA"},
+            {"ANIMAL", "CAVALO", "GATO", "CACHORRO", "ABELHA", "GALINHA", "PATO", "OVELHA", "SAPO", "URSO", "FOCA"},
+            {"COR", "AZUL", "BRANCO", "VERMELHO", "VERDE", "PRETO", "ROSA", "LARANJA", "MARROM", "CINZA", "ROXO"},
+            {"TESTE"},
+            {"NOME", "LUCAS", "BIANCA", "FELIPE", "SIMONE", "SIDNEY", "INGRID", "JOAO", "JENIFER", "RAFAEL", "LETICIA"}};
+
+    public static void main(String[] args) {
 
         menu();
     }
@@ -39,7 +43,87 @@ public class TP1 {
         System.out.println("(b) Exclusão");
         System.out.println("(c) Busca");
 
+        Scanner sc = new Scanner(System.in);
+        char choose = sc.next().charAt(0);
 
+        switch (choose) {
+            case 'a':
+                System.out.println("Qual tema deseja cadastrar?");
+                sc.nextLine();
+                String cadasTema = sc.nextLine().toUpperCase(Locale.ROOT);
+
+                boolean achouTema = true;
+                String[][] novoTemasPalavras = new String[temasPalavras.length + 1][];
+
+                for (String[] temasPalavra : temasPalavras) {
+                    achouTema = cadasTema.equals(temasPalavra[0]);
+                }
+
+                if (achouTema) {
+                    System.out.println("Tema existente!");
+                } else {
+                    for (int i = 0, k = 0; i < novoTemasPalavras.length; i++) {
+                        if (i < novoTemasPalavras.length - 1) {
+                            novoTemasPalavras[k++] = temasPalavras[i];
+                        } else {
+                            novoTemasPalavras[k++] = new String[]{cadasTema};
+                        }
+                    }
+                    temasPalavras = novoTemasPalavras;
+                }
+
+                for (String[] temasPalavra : temasPalavras) {
+                    System.out.println(Arrays.toString(temasPalavra));
+                }
+                menu();
+            case 'b':
+                System.out.println("Qual tema deseja excluir?");
+                for (int i = 0; i < temasPalavras.length; i++) {
+                    System.out.println(i + 1 + ". " + temasPalavras[i][0]);
+                }
+                int tema = sc.nextInt();
+                if (temasPalavras[tema - 1].length - 1 != 0) {
+                    System.out.println("Não foi possível excluir o tema. Verifique se existem palavras cadastradas nesse tema");
+                } else {
+                    novoTemasPalavras = new String[temasPalavras.length - 1][];
+
+                    for (int i = 0, k = 0; i < temasPalavras.length; i++) {
+                        if (i != tema - 1) {
+                            novoTemasPalavras[k++] = temasPalavras[i];
+                        }
+                    }
+                    temasPalavras = novoTemasPalavras;
+                    System.out.println("Tema excluído!");
+                }
+                menu();
+            case 'c':
+                System.out.println("Qual tema deseja buscar?");
+                sc.nextLine();
+                String checaTema = sc.nextLine().toUpperCase(Locale.ROOT);
+
+                achouTema = true;
+
+                for (String[] temasPalavra : temasPalavras) {
+                    if (checaTema.equals(temasPalavra[0])) {
+                        System.out.println("Tema existente!");
+                        System.out.println(Arrays.toString(temasPalavra));
+                        achouTema = true;
+                    } else {
+                        achouTema = false;
+                    }
+                }
+
+                if (!achouTema) {
+                    System.out.println("Tema não encontrado!");
+                }
+
+                menu();
+            default:
+                System.out.println("Opção Inválida");
+                menu();
+        }
+
+        sc.close();
     }
 
     public static void gerenciarPalavras() {
